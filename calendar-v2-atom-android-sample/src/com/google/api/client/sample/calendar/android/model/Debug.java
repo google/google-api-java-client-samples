6cea426908152e5cc13dacdf86e30dd9e6e9086b
Yaniv Inbar <yanivi@google.com>
2010-09-24 12:29:20 -0400

    remove unused code

--- calendar-v2-atom-android-sample/src/com/google/api/client/sample/calendar/android/model/Debug.java
+++ calendar-v2-atom-android-sample/src/com/google/api/client/sample/calendar/android/model/Debug.java
@@ -16,39 +16,9 @@
 
 package com.google.api.client.sample.calendar.android.model;
 
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.LogRecord;
-import java.util.logging.Logger;
-
 /**
  * @author Yaniv Inbar
  */
 public class Debug {
   public static final boolean ENABLED = false;
-
-  public static void enableLogging() {
-    if (ENABLED) {
-      Logger logger = Logger.getLogger("com.google.api.client");
-      logger.setLevel(Level.CONFIG);
-      logger.addHandler(new Handler() {
-
-        @Override
-        public void close() throws SecurityException {
-        }
-
-        @Override
-        public void flush() {
-        }
-
-        @Override
-        public void publish(LogRecord record) {
-          // default ConsoleHandler will take care of >= INFO
-          if (record.getLevel().intValue() < Level.INFO.intValue()) {
-            System.out.println(record.getMessage());
-          }
-        }
-      });
-    }
-  }
 }
