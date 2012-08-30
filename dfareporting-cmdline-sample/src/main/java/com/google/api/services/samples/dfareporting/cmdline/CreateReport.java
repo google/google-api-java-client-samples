2e2182e4883cbd723188b1267dbce6bd615e3e7c
Yaniv Inbar <yanivi@google.com>
2012-08-30 15:47:08 -0400

    [api-samples based-on-1.11] cmdline-samples: books, dfareporting, & discovery
    https://codereview.appspot.com/6492060/

--- dfareporting-cmdline-sample/src/main/java/com/google/api/services/samples/dfareporting/cmdline/CreateReport.java
+++ dfareporting-cmdline-sample/src/main/java/com/google/api/services/samples/dfareporting/cmdline/CreateReport.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2012 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -14,6 +14,7 @@
 
 package com.google.api.services.samples.dfareporting.cmdline;
 
+import com.google.api.client.util.DateTime;
 import com.google.api.services.dfareporting.Dfareporting;
 import com.google.api.services.dfareporting.model.DimensionValue;
 import com.google.api.services.dfareporting.model.Report;
@@ -22,27 +23,26 @@ import com.google.common.collect.ImmutableList;
 
 /**
  * This example creates a simple report for the given advertiser.
- *
+ * 
  * Tags: reports.insert
- *
+ * 
  * @author jdilallo@google.com (Joseph DiLallo)
  */
 public class CreateReport {
 
   /**
    * Inserts (creates) a simple report for a given advertiser.
-   *
+   * 
    * @param reporting Dfareporting service object on which to run the requests.
-   * @param userProfileId The ID number of the DFA user profile to run this
-   *     request as.
+   * @param userProfileId The ID number of the DFA user profile to run this request as.
    * @param advertiser The advertiser who the report is about.
    * @param startDate The starting date of the report.
    * @param endDate The ending date of the report.
    * @return the newly created report.
    * @throws Exception
    */
-  public static Report insert(Dfareporting reporting, Long userProfileId,
-      DimensionValue advertiser, String startDate, String endDate) throws Exception {
+  public static Report insert(Dfareporting reporting, Long userProfileId, DimensionValue advertiser,
+      String startDate, String endDate) throws Exception {
     System.out.println("=================================================================");
     System.out.printf("Creating a new report for advertiser %s%n", advertiser.getValue());
     System.out.println("=================================================================");
@@ -55,16 +55,16 @@ public class CreateReport {
 
     // Create criteria.
     Report.Criteria criteria = new Report.Criteria();
-    criteria.setDateRange(
-        new Report.Criteria.DateRange().setStartDate(startDate).setEndDate(endDate));
+    criteria.setDateRange(new Report.Criteria.DateRange().setStartDate(new DateTime(startDate))
+        .setEndDate(new DateTime(endDate)));
     criteria.setDimensions(ImmutableList.of(new SortedDimension().setName("dfa:advertiser")));
     criteria.setMetricNames(ImmutableList.of("dfa:clicks", "dfa:impressions"));
     criteria.setDimensionFilters(ImmutableList.of(advertiser));
 
     report.setCriteria(criteria);
     Report result = reporting.reports().insert(userProfileId, report).execute();
-    System.out.printf("Created report with ID \"%s\" and display name \"%s\"%n",
-        result.getId(), result.getName());
+    System.out.printf("Created report with ID \"%s\" and display name \"%s\"%n", result.getId(),
+        result.getName());
     System.out.println();
     return result;
   }
--- dfareporting-cmdline-sample/src/main/java/com/google/api/services/samples/dfareporting/cmdline/DfaReportingSample.java
+++ dfareporting-cmdline-sample/src/main/java/com/google/api/services/samples/dfareporting/cmdline/DfaReportingSample.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2012 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -15,20 +15,23 @@
 package com.google.api.services.samples.dfareporting.cmdline;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.java6.auth.oauth2.FileCredentialStore;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.HttpResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.dfareporting.Dfareporting;
 import com.google.api.services.dfareporting.model.DimensionValue;
 import com.google.api.services.dfareporting.model.DimensionValueList;
 import com.google.api.services.dfareporting.model.File;
 import com.google.api.services.dfareporting.model.Report;
 import com.google.api.services.dfareporting.model.UserProfileList;
-import com.google.api.services.samples.shared.cmdline.oauth2.LocalServerReceiver;
-import com.google.api.services.samples.shared.cmdline.oauth2.OAuth2Native;
 import com.google.common.collect.ImmutableList;
 
 import java.text.DateFormat;
@@ -47,7 +50,7 @@ import java.util.List;
  * <li>Generating a new report file from a report</li>
  * <li>Downloading the contents of a report file</li>
  * </ul>
- *
+ * 
  * @author jdilallo@google.com (Joseph DiLallo)
  */
 public class DfaReportingSample {
@@ -62,16 +65,37 @@ public class DfaReportingSample {
   private static final int MAX_LIST_PAGE_SIZE = 50;
   private static final int MAX_REPORT_PAGE_SIZE = 10;
 
+  /** Authorizes the installed application to access user's protected data. */
+  private static Credential authorize() throws Exception {
+    // load client secrets
+    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
+        JSON_FACTORY, DfaReportingSample.class.getResourceAsStream("/client_secrets.json"));
+    if (clientSecrets.getDetails().getClientId().startsWith("Enter")
+        || clientSecrets.getDetails().getClientSecret().startsWith("Enter ")) {
+      System.out.println(
+          "Enter Client ID and Secret from https://code.google.com/apis/console/?api=dfareporting "
+          + "into calendar-cmdline-sample/src/main/resources/client_secrets.json");
+      System.exit(1);
+    }
+    // set up file credential store
+    FileCredentialStore credentialStore = new FileCredentialStore(
+        new java.io.File(System.getProperty("user.home"), ".credentials/dfareporting.json"),
+        JSON_FACTORY);
+    // set up authorization code flow
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+        TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES).setCredentialStore(credentialStore).build();
+    // authorize
+    return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+  }
 
   /**
    * Performs all necessary setup steps for running requests against the API.
-   *
+   * 
    * @return An initialized Dfareporting service object.
    * @throws Exception
    */
   private static Dfareporting initializeDfareporting() throws Exception {
-    Credential credential =
-        OAuth2Native.authorize(TRANSPORT, JSON_FACTORY, new LocalServerReceiver(), SCOPES);
+    Credential credential = authorize();
 
     // Create DFA Reporting client.
     return new Dfareporting(TRANSPORT, JSON_FACTORY, credential);
@@ -79,7 +103,7 @@ public class DfaReportingSample {
 
   /**
    * Runs all the DFA Reporting API samples.
-   *
+   * 
    * @param args command-line arguments.
    */
   public static void main(String[] args) {
