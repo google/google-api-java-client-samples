ce94e4dfcefb17d723ff8444376107d198cd24c4
Yaniv Inbar <yanivi@google.com>
2012-09-04 08:51:02 -0400

    [api-samples based-on-1.11] taskqueue-cmdline-sample & storage-serviceaccount-cmdline-sample
    https://codereview.appspot.com/6499060/

--- taskqueue-cmdline-sample/src/main/java/com/google/api/services/samples/taskqueue/cmdline/TaskQueueSample.java
+++ taskqueue-cmdline-sample/src/main/java/com/google/api/services/samples/taskqueue/cmdline/TaskQueueSample.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2010 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -15,27 +15,31 @@
 package com.google.api.services.samples.taskqueue.cmdline;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.java6.auth.oauth2.FileCredentialStore;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.http.json.JsonHttpRequest;
 import com.google.api.client.http.json.JsonHttpRequestInitializer;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson.JacksonFactory;
-import com.google.api.services.samples.shared.cmdline.oauth2.LocalServerReceiver;
-import com.google.api.services.samples.shared.cmdline.oauth2.OAuth2Native;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.taskqueue.Taskqueue;
 import com.google.api.services.taskqueue.TaskqueueRequest;
 import com.google.api.services.taskqueue.TaskqueueScopes;
 import com.google.api.services.taskqueue.model.Task;
 import com.google.api.services.taskqueue.model.Tasks;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Sample which leases task from TaskQueueService, performs work on the payload of the task and then
  * deletes the task.
- *
+ * 
  * @author Vibhooti Verma
  */
 public class TaskQueueSample {
@@ -51,6 +55,31 @@ public class TaskQueueSample {
   /** Global instance of the JSON factory. */
   private static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
+  /** Authorizes the installed application to access user's protected data. */
+  private static Credential authorize() throws Exception {
+    // load client secrets
+    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
+        JSON_FACTORY, TaskQueueSample.class.getResourceAsStream("/client_secrets.json"));
+    if (clientSecrets.getDetails().getClientId().startsWith("Enter")
+        || clientSecrets.getDetails().getClientSecret().startsWith("Enter ")) {
+      System.out.println("Enter Client ID and Secret from "
+          + "https://code.google.com/apis/console/?api=taskqueue into "
+          + "taskqueue-cmdline-sample/src/main/resources/client_secrets.json");
+      System.exit(1);
+    }
+    // set up file credential store
+    FileCredentialStore credentialStore = new FileCredentialStore(
+        new File(System.getProperty("user.home"), ".credentials/taskqueue.json"),
+        JSON_FACTORY);
+    // set up authorization code flow
+    GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets,
+        Collections.singleton(TaskqueueScopes.TASKQUEUE)).setCredentialStore(credentialStore)
+        .build();
+    // authorize
+    return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+  }
+
   /**
    * You can perform following operations using TaskQueueService. 1. leasetasks 2. gettask 3. delete
    * task 4. getqueue For illustration purpose, we are first getting the stats of the specified
@@ -59,9 +88,7 @@ public class TaskQueueSample {
    */
   private static void run() throws Exception {
     // authorization
-    Credential credential = OAuth2Native.authorize(
-        HTTP_TRANSPORT, JSON_FACTORY, new LocalServerReceiver(),
-        Arrays.asList(TaskqueueScopes.TASKQUEUE));
+    Credential credential = authorize();
 
     // set up Taskqueue
     Taskqueue taskQueue = new Taskqueue.Builder(
@@ -138,7 +165,7 @@ public class TaskQueueSample {
 
   /**
    * Method that sends a get request to get the queue.
-   *
+   * 
    * @param taskQueue The task queue that should be used to get the queue from.
    * @return {@link com.google.api.services.taskqueue.model.TaskQueue}
    * @throws IOException if the request fails.
@@ -152,7 +179,7 @@ public class TaskQueueSample {
 
   /**
    * Method that sends a lease request to the specified task queue.
-   *
+   * 
    * @param taskQueue The task queue that should be used to lease tasks from.
    * @return {@link Tasks}
    * @throws IOException if the request fails.
@@ -166,7 +193,7 @@ public class TaskQueueSample {
   /**
    * This method actually performs the desired work on tasks. It can make use of payload of the
    * task. By default, we are just printing the payload of the leased task.
-   *
+   * 
    * @param task The task that should be executed.
    */
   private static void executeTask(Task task) {
@@ -176,14 +203,14 @@ public class TaskQueueSample {
 
   /**
    * Method that sends a delete request for the specified task object to the taskqueue service.
-   *
+   * 
    * @param taskQueue The task queue the specified task lies in.
    * @param task The task that should be deleted.
    * @throws IOException if the request fails
    */
   private static void deleteTask(Taskqueue taskQueue, Task task) throws IOException {
-    Taskqueue.Tasks.Delete request = taskQueue.tasks().delete(
-        projectName, taskQueueName, task.getId());
+    Taskqueue.Tasks.Delete request =
+        taskQueue.tasks().delete(projectName, taskQueueName, task.getId());
     request.execute();
   }
 }
